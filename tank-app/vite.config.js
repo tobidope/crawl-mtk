1631cb12a264952e2b89c89ac4570b92b1d7447b
Tobias Bell <tobias.bell@gmail.com>
2025-08-24 19:01:56 +0200

    Keine subpath mehr

--- tank-app/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/crawl-mtk/', // Ersetze DEIN-REPO-NAME durch den Namen deines GitHub-Repos!
-});
